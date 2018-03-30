86984733338dedec34886d6ecd7c836d66cbe495
Muthu <muthukumarsp@gmail.com>
2018-03-30 22:52:45 +1100

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+// import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { CanvasDrawService } from '../services/canvas.utilities';
 import { WritingAlphabetsPage } from '../pages/writing-alphabets/writing-alphabets';
 import { MyNavbarComponent } from '../components/navbar/navbar.component';
@@ -17,37 +17,13 @@ import { CanvasComponentModule } from '../components/canvas-component/canvas.mod
 import { NativeAudio } from '@ionic-native/native-audio';
 import { Pro } from '@ionic/pro';
 import { Injectable, Injector } from '@angular/core';
-
-Pro.init('7e72e6ba', {
-    appVersion: '0.1'
-});
+import { MyErrorHandler } from './app.error.service';
+/* 
 const cloudSettings: CloudSettings = {
     'core': {
         'app_id': '7e72e6ba'
     }
-}; 
-@Injectable()
-export class MyErrorHandler implements ErrorHandler {
-    ionicErrorHandler: IonicErrorHandler;
-
-    constructor(injector: Injector) {
-        try {
-            this.ionicErrorHandler = injector.get(IonicErrorHandler);
-        } catch (e) {
-            // Unable to get the IonicErrorHandler provider, ensure
-            // IonicErrorHandler has been added to the providers list below
-        }
-    }
-
-    handleError(err: any): void {
-        Pro.monitoring.handleNewError(err);
-        // Remove this if you want to disable Ionic's auto exception handling
-        // in development mode.
-        this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
-    }
-}
-
- 
+};  */
 
 @NgModule({
     declarations: [
@@ -60,7 +36,6 @@ export class MyErrorHandler implements ErrorHandler {
     imports: [
         BrowserModule,
         IonicModule.forRoot(MyApp),
-        CloudModule.forRoot(cloudSettings),
         CanvasComponentModule
     ],
     bootstrap: [IonicApp],
